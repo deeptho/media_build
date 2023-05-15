42f1d7f2a1af5a98ef4b312827e61e849226de18
Deep Thought <deeptho@gmail.com>
2023-05-15 23:56:33 +0200

    Disable some drivers

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ make stagingconfig
 # Disable RC/IR support
 sed -i -r 's/(^CONFIG.*_RC.*=)./\1n/g' v4l/.config
 sed -i -r 's/(^CONFIG.*_IR.*=)./\1n/g' v4l/.config
+sed -i -r 's/(^CONFIG.*_BT87X.*=)./\1n/g' v4l/.config
+sed -i -r 's/(^CONFIG.*_MXL603.*=)./\1n/g' v4l/.config
+sed -i -r 's/(^CONFIG.*_MTV23X.*=)./\1n/g' v4l/.config
+sed -i -r 's/(^CONFIG.*_GX1503.*=)./\1n/g' v4l/.config
 
 echo "V4L drivers building..."
 make -j$(nproc) &&  (echo "V4L drivers installing..." &&
